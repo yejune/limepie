9b7b0656fb4efdd847aa86b7c7ad7a09e943f68a
yejune <k@yejune.com>
2021-07-11 21:29:31 +0900

    Add Check request method when matching route in Router/Rule.php

--- src/Limepie/Router/Rule.php
+++ src/Limepie/Router/Rule.php
@@ -8,19 +8,47 @@ class Rule
         'module',
         'namespace',
         'controller',
-        // 'sub_controller',
-        // 'sub_sub_controller',
         'action',
         'path',
-        'next'
+        'next',
     ];
 
-    public static function getMatched(string $pattern, string $subject) : array
+    public static $allowMethods = [
+        'post',
+        'get',
+        'delete',
+        'put',
+    ];
+
+    public static function getMatched(string $pattern, string $subject, array $default) : array
     {
         $matches = [];
 
         if (1 === \preg_match($pattern, $subject, $matches)) {
-            if ($matches) {
+            $allowMethods = [];
+
+            if (true === isset($default['methods'])) {
+                if (true === \is_array($default['methods'])) {
+                    foreach ($default['methods'] as $method) {
+                        if ('all' == $method) {
+                            $allowMethods = static::$allowMethods;
+
+                            break;
+                        }
+                        $allowMethods[] = $default;
+                    }
+                }
+            } elseif (true === isset($default['method'])) {
+                if ('all' == $default['method']) {
+                    $allowMethods = static::$allowMethods;
+                } else {
+                    $allowMethods[] = $default['method'];
+                }
+            } else {
+                $allowMethods = static::$allowMethods;
+            }
+
+            if (true === \in_array(\strtolower($_SERVER['REQUEST_METHOD'] ?? 'get'), $allowMethods, true) && $matches) {
                 $returns               = [];
                 $returns['properties'] = [];
 
