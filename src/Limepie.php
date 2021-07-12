44ee809827403e77cab2fa82252afd83cd077818
yejune <k@yejune.com>
2021-07-12 23:05:34 +0900

    add function file_get_compressed in src/Limepie.php

--- src/Limepie.php
+++ src/Limepie.php
@@ -1837,3 +1837,27 @@ function get_parent_controller_namespace($namespace = null)
 
     return '\\' . \implode('\\', \array_slice(\explode('\\', $namespace), 0, -1)) . '\\Controller';
 }
+
+function file_get_compressed($url)
+{
+    $opts = [
+        'http' => [
+            'method' => 'GET',
+            'header' => 'Accept-Language: en-US,en;q=0.8rn'
+                        . 'Accept-Encoding: gzip,deflate,sdchrn'
+                        . 'Accept-Charset:UTF-8,*;q=0.5rn'
+                        . 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:19.0) Gecko/20100101 Firefox/19.0 FirePHP/0.4rn',
+        ],
+    ];
+
+    $context = \stream_context_create($opts);
+    $content = \file_get_contents($url, false, $context);
+
+    foreach ($http_response_header as $c => $h) {
+        if (\stristr($h, 'content-encoding') and \stristr($h, 'gzip')) {
+            $content = \gzinflate(\substr($content, 10, -8));
+        }
+    }
+
+    return $content;
+}
